7c5d4062dd579fe145464c63f3fac8f6c5fa3ed1
Lukas Gemar <lukasgemar@Lukass-MacBook-Pro.local>
2014-05-03 01:32:57 -0400

    final updates

--- Image Processing/write_each.m
+++ Image Processing/write_each.m
@@ -1,2 +1,7 @@
+% This file writes the features to a CSV file of the same name 
+
+% create a cluster object that can extract the features from an image
 clust = my_cluster(4, @rgb2hsv, @ranker.distance, false);
+
+% use the write function to write the feature outputs to the csv files
 write(each_breed, @clust.make_masks, {@extract_red, @extract_green, @extract_blue, @extract_text});
