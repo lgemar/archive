cb3abe2007ff8bd408970162237d413527eb179e
Ryan Meehan <ryanmeehan@college.harvard.edu>
2014-05-02 22:01:13 -0400

    more committs

---
+++ Image Processing/texture_sep.m
@@ -0,0 +1,12 @@
+function texture_sep(image) 
+imshow(image);
+pause(1);
+E = entropyfilt(image);
+Eim = mat2gray(E);
+imshow(Eim);
+BW1 = im2bw(Eim, .8);
+BWao = bwareaopen(BW1,2000);
+nhood = true(9);
+closeBWao = imclose(BWao,nhood);
+roughMask = imfill(closeBWao,'holes');
+imshow(roughMask);
