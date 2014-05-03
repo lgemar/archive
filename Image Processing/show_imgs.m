166ce39303134de0d7e6dd567ed9f57194c877e7
Lukas Gemar <lukasgemar@Lukass-MacBook-Pro.local>
2014-05-02 23:02:49 -0400

    final Image processing code

--- Image Processing/show_imgs.m
+++ Image Processing/show_imgs.m
@@ -1,6 +1,6 @@
 function show_imgs(varargin)
 % this function takes in any number of images and waits a specified period
-% of time between each one 
+% of time between showing each one 
 
 for i = 1:size(varargin, 2)
     if size(varargin{i}, 3) > 3
